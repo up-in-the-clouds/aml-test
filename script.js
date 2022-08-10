9ffa8397a886da60ab593658ddd155d3e5063022
Adelaida Avila C <adelaidaavilac@MacBook-Pro-de-Adelaida.local>
2022-08-10 22:19:35 +0300

    si

--- script.js
+++ script.js
@@ -1,27 +1,109 @@
-//active element menu
-// Get the container element
-var btnContainer = document.getElementById("menu_container");
+//hide mobile menu
+//"Navbar Display After Scrolling" by Jasper. https://codepen.io/jasperreyes/pen/KMXBMV
+src="https://code.jquery.com/jquery-2.2.4.min.js"
+$(document).ready(function() {
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("navigation");
+  // .scroll() creates an event when the user scrolls
+  $(window).scroll(function() {
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    // .scrollTop() retrieves vertical position of the scroll bar for the first element in a set of matched elements
+    var scroll = $(window).scrollTop();
+
+    var objectSelect = $('#mobile_header_trigger');
+
+    // .offset() retrieves current position of element relative to document
+    var objectPosition = objectSelect.offset().top;
+
+    if (scroll > objectPosition) {
+      
+      $('nav.navbar').addClass('displayNav');
+    } else {
+     
+      $('nav.navbar').removeClass('displayNav');
+
+    }
   });
-}
+});
+//close menu after click
+//https://mdbootstrap.com/support/general/nav-collapse-on-click-hide-mobile-menu-after-click-on-menu-item/
+$(document).on('click','.navbar-collapse.collapse',function(e) {
+  if( $(e.target).is('a') ) {
+      $(this).collapse('hide');
+  }
+});
 
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-// var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "250px";
-//   } else {
-//     document.getElementById("navbar").style.top = "0";
-//   }
-//   prevScrollpos = currentScrollPos;
+
+//Hamburguer Menu: CodePen/ Hamburger menu - Bootstrap 4 by Andjela 
+//https://codepen.io/AndjelaP/pen/rNxBbzR
+
+   //scrollspy
+//       $('body').scrollspy({ target: '#navbar-example' })
+//       $('[data-spy="scroll"]').each(function () {
+//   var $spy = $(this).scrollspy('refresh')
+// })
+      //scroll active
+  
+// function myFunction() {
+//   window.scrollBy(100, 100);
+//   alert("pageXOffset: " + window.pageXOffset + ", pageYOffset: " + window.pageYOffset);
 // }
+
+
+//       const sections = document.querySelectorAll("section");
+//       const navLi = document.querySelectorAll("nav .menu_container ul li");
+//       window.addEventListener("scroll", () => {
+//         let current = "";
+//         sections.forEach((section) => {
+//           const sectionTop = section.offsetTop;
+//           const sectionHeight = section.scrollHeight;
+//           if (pageYOffset >= sectionTop - sectionHeight / 5) {
+//             current = section.getAttribute("id");
+//           }
+//         });
+
+//         navLi.forEach((li) => {
+//           li.classList.remove("active");
+//           if (li.classList.contains(current)) {
+//             li.classList.add("active");
+//           }
+//         });
+//       });
+
+      //mobile
+      // function myFunction(x) {
+      //   if (x.matches) { // If media query matches
+      // //header display mobile
+      // var menuContainer = document.getElementById("menu_block");
+      // var mobileHeader = document.getElementById("myDiv");
+      // const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+      // const menuHeight = document.documentElement.scrollHeight - menuContainer.innerHeight;
+      // window.addEventListener('scroll', ()=>{
+      //  if(scrollable > 1550){
+      //    console.log("HI")
+      //   // mobileHeader.style.display=flex
+
+      //   menuHeight.onclick = console.log(menuHeight);
+      //   mobileHeader.style.visibility = "visible";
+      //  }
+      // })
+      //   }
+      //   // else {
+      //   //   document.body.style.backgroundColor = "pink";
+      //   // }
+      // }
+
+      // var x = window.matchMedia("(max-width: 800px)");
+      // myFunction(x); // Call listener function at run time
+      // x.addListener(myFunction); // Attach listener function on state changes
+
+      //hide element
+      // let textB = btnContainer.getElementsByClassName("text_box");
+      // const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+      // window.addEventListener('scroll', ()=>{
+      //  if(scrollable > 550){
+      //   document.getElementById("first").style.color="#cfe6cf"
+      //  }
+      // })
+
+      //mobile menu collapse
--- scroll.js
+++ scroll.js
@@ -1,5 +1,10 @@
-src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"
-src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"
+//Class toggles
+//ScrollMagic library https://scrollmagic.io/examples/basic/class_toggles.html
+src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"
+
+src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"
+
+
   
 
 // init controller
@@ -7,6 +12,7 @@ var controller = new ScrollMagic.Controller({
 //   globalSceneOptions: { triggerHook: 0.31 },
 });
 
+//responsive duration: https://stackoverflow.com/questions/39268491/scrollmagic-duration
 const serviHeight = $("#service").height()-20
 const homeHeight = $("#home").height()-20
 const whoHeight = $("#who").height()-20
@@ -18,7 +24,7 @@ var scene = new ScrollMagic.Scene({ triggerElement: "#home", duration: homeHeigh
   .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: "#service", duration: serviHeight  , triggerHook: 0.3})
   .setClassToggle("#menu_2", "active") // add class toggle
-  //.addIndicators() // add indicators (requires plugin)
+ // .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: "#who",  duration: whoHeight, triggerHook: 0.3 })
   .setClassToggle("#menu_3", "active") // add class toggle
@@ -26,33 +32,21 @@ new ScrollMagic.Scene({ triggerElement: "#who",  duration: whoHeight, triggerHoo
   .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: "#contact", duration:$("#contact").height(), triggerHook: 0.31 })
   .setClassToggle("#menu_4", "active") // add class toggle
-// xs // add indicators (requires plugin)
+  //.addIndicators() // add indicators (requires plugin)
   .addTo(controller);
+
 //hide text
   new ScrollMagic.Scene({ triggerElement: "#service", duration: $("#service").height() , triggerHook: 0.3 })
   .setClassToggle("#first", "hide") // add class toggle
-  //.addIndicators() // add indicators (requires plugin)
+ // .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: "#who",  duration:$("#who").height(), triggerHook: 0.30 })
   .setClassToggle("#second", "hide") // add class toggle
-  //.addIndicators() // add indicators (requires plugin)
+ // .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: "#contact", duration:"71.5%", triggerHook: 0.31 })
   .setClassToggle("#third", "hide") // add class toggle
-//   .addIndicators() // add indicators (requires plugin)
+  //.addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 
 
-
-  // get the current scroll offset for the start and end of the scene.
-// var start = scene.scrollOffset();
-// var end = scene.scrollOffset() + scene.duration();
-// console.log("the scene starts at", start, "and ends at", end);
-
-// get the current triggerElement
-// var triggerElement = scene.triggerElement();
-
-// set a new triggerElement using a selector
-// scene.triggerElement("#trigger");
-// set a new triggerElement using a DOM object
-// scene.triggerElement(document.getElementById("logo_img"));
