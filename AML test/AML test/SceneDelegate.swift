8cb2c866a88ba47c82ff1e4530a6ee14502e4b53
Adelaida Avila C <adelaidaavilac@MacBook-Pro-de-Adelaida.local>
2022-08-11 11:27:37 +0300

    upload

---
+++ AML test/AML test/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  AML test
+//
+//  Created by Adelaida Avila C on 8/10/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
